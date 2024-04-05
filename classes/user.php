bba772373812c905c71ab15485baf0349ddd9018
WildCat Studio <djacidburn1877@gmail.com>
2024-04-04 21:47:45 -0400

    Delete classes/user.php

--- classes/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User {
-    // Example method (assuming database connection is set up elsewhere)
-    public function getName() {
-        // ... (Code to fetch the username from the database)
-        return "Example User";  // Replace with actual database logic
-    }
-}
-?>
